43bb604a148b89a95eebb1fea50af8f0b93ef296
MD SAJIB <151019720+MD-SAJIB-AHAMED@users.noreply.github.com>
2024-08-26 11:36:45 +0600

    My Portfolio Updated v0.7

--- scripts.js
+++ scripts.js
@@ -20,3 +20,7 @@ window.addEventListener('scroll', () => {
     0% {opacity: 0; transform: translateY(50px);}
     100% {opacity: 1; transform: translateY(0);}
 }
+function toggleMenu() {
+    const drawer = document.getElementById('drawer');
+    drawer.classList.toggle('open');
+}
